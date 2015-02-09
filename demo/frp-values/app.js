50af61528d25bfc93c83d9bc3d3cee50b3019741
sakurafunctional <sakurafunctional@gmail.com>
2015-02-10 04:27:03 +0900

    commit

---
+++ demo/frp-values/app.js
@@ -0,0 +1,25 @@
+var ___ = require('timecomponent');
+var moment = require('moment');
+
+var timelineCapacity = moment.duration(10, 'seconds');
+
+var ___a = ___(timelineCapacity);
+var ___b = ___(timelineCapacity);
+
+var f = function()
+{
+  ___a.appear(___a.value(___('NOW')) + 1);
+};
+
+var interval = setInterval(f, 1000);
+
+___a.compute(function(x)
+{
+  ___b.appear(x * 5);
+  console.info('a: ', x);
+});
+
+___b.compute(function(x)
+{
+  console.info('b: ', x);
+});
